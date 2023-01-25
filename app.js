7df55e9efcf7f54fe1b1651e4d53690d60564488
daniyalraza88 <daniyalraza88@hotmail.com>
2023-01-25 11:01:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,201 @@
+// alert('abcd');
+//  var abc= prompt('your name please');
+
+//  document.write(abc)
+//  var name= 'Daniyal'
+//  var num =2
+//  var num2 = 3
+
+//  document.write(name+num+num2)
+
+// var studentname= prompt('Please enter your name')
+//  var Biomarks = +prompt('Enter your marks for Biology:')
+//  var Mathsmarks = +prompt('Enter your marks for Maths:')
+//  var Chemmarks = +prompt('Enter your marks for Chem:')
+//  var Phymarks = +prompt('Enter your marks for Physics:')
+//  var Arabicmarks = +prompt('Enter your marks for Arabic:')
+//  var Pakstmarks = +prompt('Enter your marks for Pakstudies:')
+
+//  var perc= Biomarks+Mathsmarks+Chemmarks+Phymarks+Arabicmarks+Pakstmarks
+//  var totalmarks = perc/6;
+
+//  document.write(totalmarks)
+
+// //  if(totalmarks)
+
+
+
+// if(totalmarks>=80 && totalmarks<101){
+//     document.write (  studentname)
+//     document.write(' <h1> A+ grade </h1> ')
+// } else if(totalmarks>=70 && totalmarks<80){
+//     document.write(studentname)
+
+//     document.write('A grade')
+// } else if(totalmarks>=60 && totalmarks<70){
+//     document.write(studentname)
+
+//     document.write('B grade')
+// } else if(totalmarks>=50 && totalmarks<60){
+//     document.write(studentname)
+
+//     document.write('C grade')
+// } else{
+//     document.write(studentname)
+
+//     document.write('You are failed')
+// }
+
+
+// document.write('abcd')
+
+// var abc = ['ali', 'abcd']
+// var abc2 = abc
+// var abc3 = abc.splice(0,1)
+// document.write(abc2)
+
+
+// var p1 = +prompt('input 1')
+// var p2= +prompt('how many tables?')
+// var p2 = prompt('input 2')
+// var p3 = prompt('input 3')
+// var p4 = prompt('input 4')
+
+// var arr = [p1,p2,p3,p4]
+// var newarr = arr. splice(0,4,p4,p3,p2,p1)
+// document.write(arr)
+
+
+// for loop
+// for(i=1;i<=p2;i++){
+//     document.write( i + "x" + p1 + "= " + i*p1 + "<br>" + "<br>")
+// }
+
+
+// while loop
+// var i=1;
+// while (i<=p2){
+//     document.write( i + "x" + p1 + "= " + i*p1 + "<br>" + "<br>")
+//     i++;
+// }
+
+
+// do while loop
+// var i=0;
+
+// do{
+//     document.write(i)
+//         i++;
+// } while(i<4)
+
+
+// var abc = ['abc','def','ghi' ]
+// for (i=0;i<abc.length;i++){
+//     document.write(abc[i])
+//     if(i<abc.length-1){
+//         document.write("-")
+//     } 
+
+
+// var countryname = ['pakistan', 'usa', 'india','england']
+// var abcd = false;
+// var user1 = prompt ('please enter your country')
+// for (i=0;i<countryname.length && abcd == false ;i++){
+//     if (user1 == countryname[i] ){
+//         document.write('country found')
+//         abcd == true;
+//        break;
+//     }  else{
+// document.write('country not found')
+//     }
+// }
+
+
+// var abc = '123'
+// var def = 123
+
+// if(abc==def){
+//     document.write('ok hay')
+// }
+
+
+// function basic
+// var a = prompt('hello world')
+
+// function add () {
+//     // document.write(a);
+//     return 5;
+// }
+
+// document.write(add())
+
+
+
+// calculator using functions
+// function add (a,b){
+//     return a+b
+// }
+
+// function sub (a,b){
+//     return a-b
+// }
+
+// function mul (a,b){
+//     return a*b
+// }
+
+// function div (a,b){
+//     return a/b
+// }
+
+
+// var val = +prompt('enter your number 1')
+// var sign = prompt('enter the sign')
+// var val2 = +prompt('enter your number 2')
+
+
+// if (sign == '+'){
+//     document.write(add(val,val2))
+// } else if (sign == '-'){
+//     document.write(sub(val,val2))
+// }
+// else
+// if (sign == '*'){
+//     document.write(mul(val,val2))
+// }
+// else
+// if (sign == '/'){
+//     document.write(div(val,val2))
+// }
+
+
+
+
+// function onimg (){
+//     document.getElementById('imgon').src = "https://thumbs.dreamstime.com/b/idea-light-bulb-line-icon-shining-effect-yellow-color-switch-lamp-business-success-concept-flat-design-white-background-i-97725453.jpg";
+
+// }
+
+// function offimg (){
+//     document.getElementById('imgon').src = "http://puzzles.nigelcoldwell.co.uk/images/light-bulboff.png" ;
+
+// }
+
+
+// arr methods
+// var abc = prompt('enter your input');
+// var arr = ['dog','cat','fish'];
+// arr.splice(0,2,'mouse')
+// document.write(arr)
+// document.write("<br>")
+// arr.unshift('ahvjhv')
+
+// document.write(arr)
+// document.write("<br>")
+
+// var abc = arr.slice(0,2)
+// document.write(abc)
+
+
+
+
